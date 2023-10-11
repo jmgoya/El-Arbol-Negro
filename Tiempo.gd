0087c4b7e92cb4860ef4706b0dbc9ad0c52ab112
jmgoya <jmgoya@gmail.com>
2023-10-11 01:34:33 -0300

    Update Tiempo.gd

--- Tiempo.gd
+++ Tiempo.gd
@@ -1,16 +1,27 @@
 extends Timer
 
-@export var dia:int = 0
-@export var hora:float = 1.0
-@export var lapso:float = 8.0
-var delta_tiempo:float = 0.2
+@export var hora: float = 8.0
+@export var dia: int = 1
+@export var velocidad_reloj: float = 1.0
+@export var delta_tiempo: float = 0.2
 
-func _ready():
-	self.wait_time = lapso
+
 
 func _on_timeout():
-	print (hora)
-	hora = hora + (lapso * delta_tiempo)
-	if hora>= 24:
+	hora = hora + (delta_tiempo * velocidad_reloj)
+	if (hora >= 24):
 		hora = 0
-	Eventos.emit_signal("tiempo_real",  dia, hora,0)
+		dia += 1
+	Eventos.emit_signal("tiempo", dia, hora, color_cielo(hora) )
+
+func color_cielo(hora):
+	var color: Color
+	if hora >= 7:
+		color = Color(1,0,1)
+	if hora >= 10:
+		color = Color(1,1,1)
+	if hora >= 17:
+		color = Color(1,0,0.4)
+	if (hora >= 20 or hora <= 6):
+		color = Color(0.01,0.01,0.2)
+	return color
